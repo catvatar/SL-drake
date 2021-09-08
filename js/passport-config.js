0d858b4f0d0bc0676737e10406252c81c8488815
catvatar <olekgogol@gmail.com>
2021-09-08 03:11:32 +0200

    .

--- js/passport-config.js
+++ js/passport-config.js
@@ -2,7 +2,7 @@ const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail) {
-  const authenticateUser = (email, password, done) => {
+  const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email)
     if (user == null) {
       return done(null, false, { message: 'No user' })
@@ -18,7 +18,7 @@ function initialize(passport, getUserByEmail) {
       return done(e)
     }
   }
-  passport.use(new LocalStrategy({ usernameField: 'email' }), authenticateUser)
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
   passport.serializeUser((user, done) => { })
   passport.deserializeUser((id, done) => { })
 }
