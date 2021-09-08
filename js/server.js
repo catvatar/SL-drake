9c6ca304f5311b5a8b34056d378fe14b99a154ca
catvatar <olekgogol@gmail.com>
2021-09-08 02:04:01 +0200

    .

--- js/server.js
+++ js/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/mybrary', { useNewUrlParser: true })
 mongoose.connection.once('open', () => console.log('not connected'))
 
 const User = require('./data/users.js')
