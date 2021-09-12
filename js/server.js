c1e39e5cc90289fac26128b1328155406255dcab
catvatar <olekgogol@gmail.com>
2021-09-12 18:08:44 +0200

    tests

--- js/server.js
+++ js/server.js
@@ -1,68 +1,82 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
 
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
-const initializePassport = require('./passport-config')
+const initializePassport = require('./passport-config');
 initializePassport(
   passport,
   async email => User.find({ email: email }),
   async id => User.find({ _id: id })
-)
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true })
-mongoose.connection.once('open', () => console.log('connected'))
+);
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log('connected'));
 
-const User = require('./data/users.js')
+const User = require('./data/users.js');
 
-app.use(express.json())
-app.use(flash())
+app.use(express.json());
+app.use(flash());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/users', async (req, res) => {
+  User.findOne({ email: req.body.email }, function (err, result) {
+    if (err) {
+      console.log('cought a penis get');
+      res.status(333).send();
+    } else if (!result) {
+      res.send("No such user");
+    } else {
+      res.json(result);
+    }
+  })
+
+
+});
+
+app.post('/register', async (req, res) => {
   try {
-    const users = await User.find()
-    res.json(users)
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    User.findOne({ email: req.body.email }, function (err, result) {
+      if (err) {
+        console.log('cought a penis post');
+      } else if (!result) {
+        const user = new User({ username: req.body.username, email: req.body.email, passwordHash: hashedPassword })
+
+        user.save(function (err) {
+          if (err) console.log(err);
+        })
+
+        res.status(201).send();
+
+      } else {
+        res.send("User already exist");
+      }
+    });
+
   } catch {
-    console.log('cought a penis get')
-    res.status(500).send()
+    res.status(500).send();
   }
-
-})
-
-app.post('/users/register', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-    const user = new User({ username: req.body.username, email: req.body.email, passwordHash: hashedPassword })
-
-    user.save(function (err) {
-      if (err) console.log(err)
-    })
-
-    res.status(201).send()
-  } catch {
-    res.status(500).send()
-  }
-})
+});
 
 app.post('/users/login', passport.authenticate('local', {
   function(req, res) {
-    res.status(100).send()
+    res.status(100).send();
   },
   failureFlash: true
-}))
+}));
 
-app.listen(3000)
+app.listen(3000);
